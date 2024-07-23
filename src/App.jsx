31c2a372ccd8b7157cf86ab93da5c57b8391e751
FrancoDominguez <franco@wearecws.com>
2024-07-23 10:51:25 -0400

    setting up vite+react+tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen text-red-500">
+      hello
+    </div>
+  );
+}
+export default App;
